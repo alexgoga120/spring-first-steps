8af626faf731f65237e54caf1e1ccdae812e6e9b
alex <alex.satoritech@gmail.com>
2023-01-30 20:19:18 -0600

    Se añade parcialmente Aws S3 buckets

---
+++ src/main/java/com/example/springfirststeps/providers/adapters/representation/BucketObjectRepresentaion.java
@@ -0,0 +1,9 @@
+package com.example.springfirststeps.providers.adapters.representation;
+
+import lombok.Data;
+
+@Data
+public class BucketObjectRepresentaion {
+    private String objectName;
+    private String text;
+}
